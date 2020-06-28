4e77cdcb9da045ea4e21f800179b7ac748936ba1
simba-luo <r.puppy@qq.com>
2020-06-28 17:14:51 +0800

    learn

--- index.js
+++ index.js
@@ -428,62 +428,6 @@ function Update()
 }
     
 /////////////////////////////////////////////////////////////////////////////////////
-// mouse input
-/////////////////////////////////////////////////////////////////////////////////////
-
-let mouseDown       = 0; 
-let mouseWasPressed = 0;
-let mouseUpFrames   = 0;
-let mouseX          = 0;
-let mouseLockX      = 0;
-let touchMode       = 0;
-    
-/**
- * 释放鼠标
- */
-window.onmouseup   = e => mouseDown = 0;
-
-/**
- * 按下鼠标
- */
-window.onmousedown = e => {
-
-    if (mouseWasPressed)
-        mouseDown = 1;
-    mouseWasPressed = 1;
-    if (usePointerLock && e.button == 0 && document.pointerLockElement !== c)
-    {
-        c.requestPointerLock();
-        mouseLockX = 0;
-    }
-};
-
-onmousemove = e => 
-{
-    if (!usePointerLock)
-    {
-        mouseX = e.x/window.innerWidth*2-1
-        return;
-    }
-    
-    if (document.pointerLockElement !== c)
-        return;
-    
-    // adjust for pointer lock 
-    mouseLockX += e.movementX;
-    mouseLockX = Clamp(mouseLockX, -window.innerWidth/2,  window.innerWidth/2);
-    
-    // apply curve to input
-    const inputCurve = 1.5;
-    mouseX = mouseLockX;
-    mouseX /= window.innerWidth/2;
-    mouseX = Math.sign(mouseX) * (1-(1-Math.abs(mouseX))**inputCurve);
-    mouseX *= window.innerWidth/2;
-    mouseX += window.innerWidth/2;
-    mouseX = mouseX/window.innerWidth*2-1
-}
-
-/////////////////////////////////////////////////////////////////////////////////////
 // touch control
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -593,32 +537,6 @@ function UpdateFps()
     context.fillText(averageFps|0,
             c.width  - 90,
             c.height - 40);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////
-// keyboard control
-/////////////////////////////////////////////////////////////////////////////////////
-
-let inputIsDown    = [];
-let inputWasDown   = [];
-let inputWasPushed = [];
-
-
-/**
- * 按下按键
- */
-window.onkeydown = e => inputIsDown[e.keyCode] = 1;
-
-/**
- * 释放按键
- */
-window.onkeyup   = e => inputIsDown[e.keyCode] = 0;
-
-
-function UpdateInput()
-{
-    inputWasPushed = inputIsDown.map((e,i) => e && !inputWasDown[i]);
-    inputWasDown = inputIsDown.slice();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
